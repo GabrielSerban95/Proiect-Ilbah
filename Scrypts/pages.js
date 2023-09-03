eb7f8fea4aba44569c5dd1a90693c84553692ff9
GabrielSerban95 <124520766+GabrielSerban95@users.noreply.github.com>
2023-09-03 20:43:48 +0300

    diferite modificari la pagina de preturi

---
+++ Scrypts/pages.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $(".btn-poze").click(function () {
+        $(this).parents(".card-inner").toggleClass("flipped");
+        $(".card-back-fotografii").show();
+        $(".card-back-filmari").hide();
+    });
+
+    $(".btn-filme").click(function () {
+        $(this).parents(".card-inner").toggleClass("flipped")
+        $(".card-back-fotografii").hide();
+        $(".card-back-filmari").show();
+    });
+
+    $(".btn-reset").click(function () {
+        $(this).parents(".card-inner").toggleClass("flipped")
+    });
+
+
+
+    // Swiper JS code
+    var swiper = new Swiper(".slider-content", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlide: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+
+            1182: {
+                slidesPerView: 2,
+            },
+
+            1560: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
+
+
+
